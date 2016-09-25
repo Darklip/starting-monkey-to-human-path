146846ddae72c2ea25087a8bb52b6bfdfcfe8c1a
Vyacheslav Lipatkin <>
2016-09-25 19:54:55 +0400

    Added entry point

---
+++ Application.java
@@ -0,0 +1,7 @@
+package RPIS41.Lipatkin.wdad;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I'm Vyacheslav Lipatkin, and I'm not a monkey!");
+    }
+}
