f6105c8c385561d39834034e83aa85ab47211e12
Vyacheslav Lipatkin <1darkpower1@gmail.com>
2016-10-07 14:49:33 +0400

    fixed mistype

--- utils/PreferencesConstantManager.java
+++ utils/PreferencesConstantManager.java
@@ -6,7 +6,7 @@ public interface PreferencesConstantManager {
     public static final String REGISTRY_ADDRESS = "appconfig.rmi.server.registry.registryaddress";
     public static final String REGISTRY_PORT = "appconfig.rmi.server.registry.registryport";
     public static final String POLICY_PATH = "appconfig.rmi.client.policypath";
-    public static final String USE_BASE_CODE_ONLY = "appconfig.rmi.client.usecodebaseonly";
+    public static final String USE_CODE_BASE_ONLY = "appconfig.rmi.client.usecodebaseonly";
     public static final String CLASS_PROVIDER = "appconfig.rmi.classprovider";
     
 }
